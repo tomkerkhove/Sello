787e6c445a20d4e4d778e83b5cf66aa01e2838a3
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-11-11 15:27:41 +0100

    Initial setup for integration testing

---
+++ src/Sello.Tests.Integration/ProductsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Sello.Tests.Integration
+{
+    [Category("Integration")]
+    [Category("Smoke")]
+    public class ProductsTests
+    {
+        [Test]
+        public void Products_ListAllProducts_ShouldReturnHttpOk()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Pass();
+        }
+    }
+}
