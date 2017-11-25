a1c4005cf335c3123e8037799f7b4cca5cc0135b
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-11-25 22:01:26 +0100

    Remove domain models, get product details & add product (#16)
    * WIP
    
    * Remove domain models & new operations for products

--- src/Sello.Api/Controllers/ProductsController.cs
+++ src/Sello.Api/Controllers/ProductsController.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using AutoMapper;
 using Sello.Api.Contracts;
-using Sello.Data.Managers;
+using Sello.Data.Repositories;
+using Sello.Datastore.SQL.Model;
 using Swashbuckle.Swagger.Annotations;
 
 namespace Sello.Api.Controllers
@@ -13,7 +14,7 @@ namespace Sello.Api.Controllers
     [RoutePrefix("api/v1")]
     public class ProductsController : ApiController
     {
-        private readonly ProductsManager _productsManager = new ProductsManager();
+        private readonly ProductsRepository _productsRepository = new ProductsRepository();
 
         /// <summary>
         ///     Gets a list of all products
@@ -25,11 +26,54 @@ namespace Sello.Api.Controllers
             "The request could not be completed successfully, please try again.")]
         public async Task<IHttpActionResult> Get()
         {
-            var storedProducts = await _productsManager.GetAsync();
+            var storedProducts = await _productsRepository.GetAsync();
 
             var products = storedProducts.Select(Mapper.Map<ProductContract>);
 
             return Ok(products);
+        }
+
+        /// <summary>
+        ///     Gets the details for a specific product in the catalog
+        /// </summary>
+        [HttpGet]
+        [Route("products/{productId}")]
+        [SwaggerResponse(HttpStatusCode.OK, "Details about a product", typeof(ProductContract))]
+        [SwaggerResponse(HttpStatusCode.NotFound, "Product not found")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError,
+            "The request could not be completed successfully, please try again.")]
+        public async Task<IHttpActionResult> Get(int productId)
+        {
+            var storedProduct = await _productsRepository.GetAsync(productId);
+            if (storedProduct == null)
+                return NotFound();
+
+            var product = Mapper.Map<ProductContract>(storedProduct);
+
+            return Ok(product);
+        }
+
+        /// <summary>
+        ///     Add a new product to the catalog a list of all products
+        /// </summary>
+        [HttpPost]
+        [Route("products")]
+        [SwaggerResponse(HttpStatusCode.Created, "A list of all products", typeof(ProductContract))]
+        [SwaggerResponse(HttpStatusCode.BadRequest, "No valid product was specified.")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError,
+            "The request could not be completed successfully, please try again.")]
+        public async Task<IHttpActionResult> Post(ProductContract newProduct)
+        {
+            var product = Mapper.Map<Product>(newProduct);
+            var storedProduct = await _productsRepository.AddAsync(product);
+
+            var resourceLocation = ComposeResourceLocation(storedProduct.Id);
+            return Created(resourceLocation, newProduct);
+        }
+
+        private string ComposeResourceLocation(int resourceId)
+        {
+            return $"{Request.RequestUri.OriginalString}/{resourceId}";
         }
     }
 }
