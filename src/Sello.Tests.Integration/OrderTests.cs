7e5646ae4f04ad560b9a4dd7ae0d819dc5b0a46d
Tom Kerkhove <kerkhove.tom@gmail.com>
2017-12-15 18:35:57 +0100

    Add validation tests

--- src/Sello.Tests.Integration/OrderTests.cs
+++ src/Sello.Tests.Integration/OrderTests.cs
@@ -19,7 +19,7 @@ namespace Sello.Tests.Integration
             // Arrange
             const string ordersUrl = "order";
             const string customerFirstName = "Tom";
-            const string customerLastName = "Tom";
+            const string customerLastName = "Kerkhove";
             const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
             var productToBuy = await GetProductFromCatalogAsync();
 
@@ -55,6 +55,222 @@ namespace Sello.Tests.Integration
             Assert.AreEqual(customerFirstName, orderConfirmation.Order.Customer.FirstName);
             Assert.AreEqual(customerLastName, orderConfirmation.Order.Customer.LastName);
             Assert.AreEqual(customerEmailAddress, orderConfirmation.Order.Customer.EmailAddress);
+        }
+
+        [Test]
+        public async Task Orders_CreateForExistingProductWithChangedPrice_ShouldReturnHttpBadRequest()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            const string customerFirstName = "Tom";
+            const string customerLastName = "Kerkhove";
+            const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
+            var productToBuy = await GetProductFromCatalogAsync();
+            productToBuy.Price = productToBuy.Price * 2;
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = productToBuy
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Orders_CreateForExistingProductWithChangedDescription_ShouldReturnHttpBadRequest()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            const string customerFirstName = "Tom";
+            const string customerLastName = "Kerkhove";
+            const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
+            var productToBuy = await GetProductFromCatalogAsync();
+            productToBuy.Description = "Altered description";
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = productToBuy
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Orders_CreateForExistingProductWithChangedName_ShouldReturnHttpBadRequest()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            const string customerFirstName = "Tom";
+            const string customerLastName = "Kerkhove";
+            const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
+            var productToBuy = await GetProductFromCatalogAsync();
+            productToBuy.Name = "Altered name";
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = productToBuy
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Orders_CreateForExistingProductWithoutCustomerFirstName_ShouldReturnHttpBadRequest()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            var customerFirstName = string.Empty;
+            const string customerLastName = "Kerkhove";
+            const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
+            var productToBuy = await GetProductFromCatalogAsync();
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = productToBuy
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Orders_CreateForExistingProductWithoutCustomerLastName_ShouldReturnHttpBadRequest()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            const string customerFirstName = "Tom";
+            var customerLastName = string.Empty;
+            const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
+            var productToBuy = await GetProductFromCatalogAsync();
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = productToBuy
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Orders_CreateForExistingProductWithoutCustomerEmailAddress_ShouldReturnHttpBadRequest()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            const string customerFirstName = "Tom";
+            const string customerLastName = "Kerkhove";
+            var customerEmailAddress = string.Empty;
+            var productToBuy = await GetProductFromCatalogAsync();
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = productToBuy
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Orders_CreateForNotExistingProduct_ShouldReturnHttpNotFound()
+        {
+            // Arrange
+            const string ordersUrl = "order";
+            const string productId = "I-DO-NOT-EXIST";
+            const string productName = "Validation Product";
+            const string productDescription = "Product created by Integration Test, however it should never make it in";
+            const double productPrice = 100;
+            const string customerFirstName = "Tom";
+            const string customerLastName = "Tom";
+            const string customerEmailAddress = "Tom.Kerkhove@codit.eu";
+
+            var customer = new CustomerContract
+            {
+                FirstName = customerFirstName,
+                LastName = customerLastName,
+                EmailAddress = customerEmailAddress
+            };
+            var product = new ProductInformationContract
+            {
+                Id = productId,
+                Name = productName,
+                Description = productDescription,
+                Price = productPrice
+            };
+            var order = new OrderContract
+            {
+                Customer = customer,
+                Product = product
+            };
+
+            // Act
+            var response = await _selloService.PostResponseAsync(ordersUrl, order);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
         }
 
         private static async Task<ProductInformationContract> GetProductFromCatalogAsync()
