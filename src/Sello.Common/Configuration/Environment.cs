45ae7c8f89f808a1f6d7c7df1bfe80f205de413e
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-01-07 19:02:16 +0100

    Add parameters to inject configuration

--- src/Sello.Common/Configuration/Environment.cs
+++ src/Sello.Common/Configuration/Environment.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sello.Common.Configuration
+﻿namespace Sello.Common.Configuration
 {
     public enum Environment
     {
